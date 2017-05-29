eb91710d97a99bdca45a635c64b14f6e38b664e7
RedRect <chenmrkent@hotmail.com>
2017-05-30 05:36:06 +0800

    reDo

---
+++ Project/BookOrderingSystem/app/src/main/java/comp3350project/bookorderingsystem/MainActivity.java
@@ -0,0 +1,27 @@
+package comp3350project.bookorderingsystem;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "comp3010_group10.bookordering.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendSearch(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this,SearchActivity.class);
+        EditText editText = (EditText) findViewById(R.id.searchText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}

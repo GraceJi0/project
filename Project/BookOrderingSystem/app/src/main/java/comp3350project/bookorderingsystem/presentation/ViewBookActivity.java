c823213fd5827494c74e9a8ee0486166f7050d8a
GraceJi0 <jid@myumanitoba.ca>
2017-06-17 15:50:54 -0500

    fix my account activity bug
    fix checkbox bug

--- Project/BookOrderingSystem/app/src/main/java/comp3350project/bookorderingsystem/presentation/ViewBookActivity.java
+++ Project/BookOrderingSystem/app/src/main/java/comp3350project/bookorderingsystem/presentation/ViewBookActivity.java
@@ -112,8 +112,10 @@ public class ViewBookActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
+                Log.d("-------------","***************");
                 Intent intent = new Intent(ViewBookActivity.this, MyAccountActivity.class);
                 intent.putExtra("name",accountName);
+                startActivity(intent);
             }
         });
 

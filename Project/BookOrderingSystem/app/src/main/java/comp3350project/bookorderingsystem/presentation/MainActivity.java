f0a0ce80366a00a9a8dd9f02cc90176e5d426b97
FTDlee <lix34528@myumanitoba.ca>
2017-06-20 18:13:41 -0500

    sign up

--- Project/BookOrderingSystem/app/src/main/java/comp3350project/bookorderingsystem/presentation/MainActivity.java
+++ Project/BookOrderingSystem/app/src/main/java/comp3350project/bookorderingsystem/presentation/MainActivity.java
@@ -3,10 +3,12 @@ package comp3350project.bookorderingsystem.presentation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import comp3350project.bookorderingsystem.R;
 import comp3350project.bookorderingsystem.application.Main;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         Main.startUp();
         setContentView(R.layout.activity_main);
         setButton();
+        signIn();
     }
 
     /*public void sendSearch(View view)
@@ -34,7 +37,43 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }*/
 
+   public void signIn()
+   {
+       Button showSignIn=(Button)findViewById(R.id.signinBut);
+       showSignIn.setOnClickListener(new View.OnClickListener()
+       {
+           public void onClick(View view)
+           {
+               AlertDialog.Builder loginBuilder=new AlertDialog.Builder(MainActivity.this);
+               View signinView=getLayoutInflater().inflate(R.layout.activityt_signin,null);
+               final EditText signinAccount=(EditText) signinView.findViewById(R.id.signin_account);
+               final EditText signinPassword=(EditText) signinView.findViewById(R.id.signin_password);
+               Button signinButton=(Button) signinView.findViewById(R.id.signin_button);
 
+               signinButton.setOnClickListener(new View.OnClickListener()
+               {
+                   public void onClick(View view)
+                   {
+                       if(!signinAccount.getText().toString().isEmpty() && !signinPassword.getText().toString().isEmpty())
+                       {
+                           Toast.makeText(MainActivity.this,
+                                   "Login successful",
+                                   Toast.LENGTH_SHORT).show();
+                       }
+                       else
+                       {
+                           Toast.makeText(MainActivity.this,
+                                   "please fill account or password",
+                                   Toast.LENGTH_SHORT).show();
+                       }
+                   }
+               });
+               loginBuilder.setView(signinView);
+               AlertDialog dialog=loginBuilder.create();
+               dialog.show();
+           }
+       });
+   }
     public void setButton()
     {
 
---
+++ Project/BookOrderingSystem/app/src/main/java/comp3350project/bookorderingsystem/presentation/SignInActivity.java
@@ -0,0 +1,65 @@
+package comp3350project.bookorderingsystem.presentation;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import comp3350project.bookorderingsystem.R;
+import comp3350project.bookorderingsystem.application.Main;
+
+/**
+ * Created by lee on 2017/6/20.
+ */
+
+public class SignInActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button showSignIn=(Button)findViewById(R.id.signinBut);
+        showSignIn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                AlertDialog.Builder loginBuilder=new AlertDialog.Builder(SignInActivity.this);
+                View signinView=getLayoutInflater().inflate(R.layout.activityt_signin,null);
+                final EditText signinAccount=(EditText) signinView.findViewById(R.id.signin_account);
+                final EditText signinPassword=(EditText) signinView.findViewById(R.id.signin_password);
+                Button signinButton=(Button) signinView.findViewById(R.id.signin_button);
+
+                signinButton.setOnClickListener(new View.OnClickListener()
+                {
+                   public void onClick(View view)
+                   {
+                       if(!signinAccount.getText().toString().isEmpty() && !signinPassword.getText().toString().isEmpty())
+                       {
+                           Toast.makeText(SignInActivity.this,
+                                   "Login successful",
+                                   Toast.LENGTH_SHORT).show();
+                       }
+                       else
+                       {
+                           Toast.makeText(SignInActivity.this,
+                                   "please fill account or password",
+                                   Toast.LENGTH_SHORT).show();
+                       }
+                   }
+                });
+                loginBuilder.setView(signinView);
+                AlertDialog dialog=loginBuilder.create();
+                dialog.show();
+            }
+        });
+
+
+    }
+
+}
